56996f472dfb3bb919bc006a70eac02355b38342
glasses <glasseszinzet@gmail.com>
2021-09-04 21:59:36 +0000

    initial commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+    public static void main(String[] args){
+        int n = 3;
+        n++;
+        System.out.println("n == " + n);
+        n--;
+        System.out.println("n == " + n);
+
+    }
+}
